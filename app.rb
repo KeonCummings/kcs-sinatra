51fe611ba27a6fe03b4db369eb75496349761586
Keon Cummings <Keon.cummings@gmail.com>
2019-08-12 13:31:02 -0500

    updated program pages

--- app.rb
+++ app.rb
@@ -10,3 +10,18 @@ get "/" do
   erb :index
 end
 
+get "/infants" do
+  erb :infants
+end
+
+get "/toddlers" do
+  erb :toddlers
+end
+
+get "/twos" do
+  erb :twos
+end
+
+get "/preschool" do
+  erb :preschool
+end
